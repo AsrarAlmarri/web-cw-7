15861ed6f706615b153a9653035f95fb8a505285
AsrarAlmarri <76258365+AsrarAlmarri@users.noreply.github.com>
2021-01-05 21:38:43 +0300

    done

--- question1.js
+++ question1.js
@@ -1,1 +1,37 @@
 // قم بحل السؤال الأول هنا
+
+
+// 1
+let pets = [`cat`,`dog`,`snake`,`squirrel`,`wolf`];
+
+console.log(pets);
+pets.pop();
+console.log(pets);
+pets.push(`hamster`);
+console.log(pets);
+
+
+
+ let kicked =   pets.splice(2,2,`fish`);
+
+console.log(pets);
+
+
+
+//   2
+
+let PET = {
+
+    name:`cat`,
+    age:5,
+    preed:`scoth`,
+    
+
+};
+
+PET.emoji = `🐱`;
+
+
+
+console.log(PET);
+console.log(`age is = ${PET.age}`);
